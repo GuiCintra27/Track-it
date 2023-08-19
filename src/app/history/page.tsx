d6543cea72fb99186405a152bf2ea0d0696d7e94
Cintra <gui7lk@gmail.com>
2023-08-19 19:40:58 -0300

    feat: authentication on history page

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { useAuth } from "@/hooks/useAuth";
 import { Title } from "../../components/UI/title";
-import { AppLayout } from "../../components/common/layouts/appLayout";
 import { Footer } from "../../components/common/footer";
 import { Header } from "../../components/common/header";
+import { AppLayout } from "../../components/common/layouts/appLayout";
 
 export default function Home() {
+  useAuth();
   return (
     <AppLayout>
       <Header />
