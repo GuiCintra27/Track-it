dd3102236e50ec91601d7c2bfb848013ca9b986e
Cintra <gui7lk@gmail.com>
2023-09-16 12:33:28 -0300

    fix: i18next load navigator language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,12 @@ import { Provider } from "react-redux";
 import { Lexend_Deca } from "next/font/google";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import "@/lib/i18n";
+import i18n from "@/lib/i18n";
 import { store } from "@/global/store";
 import GlobalStyle from "@/global/globalStyle";
 import StyledComponentsRegistry from "../lib/registry";
+import { useEffect } from "react";
+import { I18nextProvider } from "react-i18next";
 
 const lexendDeca = Lexend_Deca({
   weight: ["400", "500", "600", "700"],
@@ -21,13 +23,24 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  if (typeof window !== "undefined" && typeof navigator !== "undefined") {
+    useEffect(() => {
+      const storageSettings = window.localStorage.getItem("settings");
+
+      if (storageSettings) {
+        const settings = JSON.parse(storageSettings);
+        i18n.changeLanguage(settings.language);
+      } else i18n.changeLanguage(navigator.language);
+    }, [window, navigator]);
+  }
+  
   return (
     <html lang="pt-br" suppressHydrationWarning={true}>
       <body className={lexendDeca.className}>
         <QueryClientProvider client={queryClient}>
           <StyledComponentsRegistry>
             <Provider store={store}>
-              {children}
+              <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
             </Provider>
             <GlobalStyle />
           </StyledComponentsRegistry>
--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -4,28 +4,23 @@ import { initReactI18next } from "react-i18next";
 import global_en from "./translations/en.json";
 import global_pt from "./translations/pt.json";
 
-let storageSettings : null | string = null
-
-if(typeof window !== "undefined") storageSettings = window.localStorage.getItem("settings");
-
-let settings : {theme: string, language: string} = {theme: "", language: ""}
-
-if(storageSettings) settings = JSON.parse(storageSettings)
-
 i18n
-  // pass the i18n instance to react-i18next.
-  .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
-  .init({
-    lng: settings.language ? settings.language : navigator.language,
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
-    },
-    resources: {
-      en: global_en,
-      pt: global_pt,
-    },
-  });
+    // pass the i18n instance to react-i18next.
+    .use(initReactI18next)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+      fallbackLng: "en",
+      lng: "pt",
+      preload: ["en"],
+      interpolation: {
+        escapeValue: false, // not needed for react as it escapes by default
+      },
+      resources: {
+        en: global_en,
+        pt: global_pt,
+      },
+    });
 
 export default i18n;
+
