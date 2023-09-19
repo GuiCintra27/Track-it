0271fdca8d5b549606b947584f3781512f08d35b
Cintra <gui7lk@gmail.com>
2023-09-19 17:09:09 -0300

    feat: update axios instance on logout

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,10 +1,13 @@
-import { userInfo } from "@/components/infra/storage/user-slice";
 import { useRouter } from "next/navigation";
+
+import { instance } from "@/services/instance";
+import { userInfo } from "@/components/infra/storage/user-slice";
 
 export function useLogout() {
   localStorage.removeItem(userInfo.userApiData);
   localStorage.removeItem(userInfo.userLocalData);
-  
+  delete instance.defaults.headers["Authorization"];
+
   const router = useRouter();
   router.push("/sign-in");
 }
