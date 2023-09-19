3bdae02309649e1217e7370306e0f1f5c9f24cae
Cintra <gui7lk@gmail.com>
2023-09-19 17:25:26 -0300

    feat: change default user image

--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -11,7 +11,6 @@ export async function signUp({ name, email, password }: SignUpData) {
     name,
     email,
     password,
-    image:
-      "https://i.pinimg.com/236x/8c/e5/3f/8ce53f52b2dd9fb20d125c1e7b1e0425.jpg",
+    image: "https://i.imgur.com/r7JNyeg.png",
   });
 }
